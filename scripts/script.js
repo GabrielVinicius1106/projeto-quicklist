6f16518e152d25fedfb768010e15412f41873639
Gabriel Vinícius da Cruz <gabrielviniciusdacruz546@gmail.com>
2025-05-14 10:56:29 -0300

    iniciando script para adicionar item à lista

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-console.log(document.getElementById('check').value)
+document.querySelector("#add-item").addEventListener("click", () => {
+
+    let item = document.querySelector("#new-item").value
+    let itemList = document.querySelector(".items")
+    let divItem = document.createElement("div")
+    divItem.classList.add("item")
+
+    itemList.appendChild(divItem)
+})
