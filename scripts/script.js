249dd85c44c8942dc7c44e120632caf292639933
Gabriel Vinícius da Cruz <gabrielviniciusdacruz546@gmail.com>
2025-05-15 16:57:03 -0300

    reiniciar script para adição e remoção de itens

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#add-item").addEventListener("click", () => {
-
-    let item = document.querySelector("#new-item").value
-    let itemList = document.querySelector(".items")
-    let divItem = document.createElement("div")
-    divItem.classList.add("item")
-
-    itemList.appendChild(divItem)
-})
